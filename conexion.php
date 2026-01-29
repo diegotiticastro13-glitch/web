684ea68bdc32e8b3fc8fee13a5c0467ebb2a8c6c
diegotiticastro13-glitch <diego.titicastro13@gmail.com>
2026-01-29 10:04:13 -0700

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "login_bd";
+$host = "localhost"; // A veces el hosting te da una IP o nombre distinto
+$user = "Dcastro130731"; // Usuario creado en el panel del hosting
+$pass = "Pompillas@20082"; // Contraseña de la base de datos
+$db = "login_bd"; // Nombre de la BD en el hosting
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -10,3 +10,4 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
+
